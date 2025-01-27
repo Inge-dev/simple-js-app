f677222462ed22b69da2d10a75180256cdd05e62
Inge-dev <ingelewerentz@gmail.com>
2025-01-27 12:23:39 -0800

    Defined pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world')
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList= [
+    {name: 'Jigglypuff', height: 5, type: ['poison', 'steel']},
+    {name: 'Butterfree', height: 10, type: ['rock', 'electric']},
+    {name: 'Charmander', height: 6, type: ['ground', 'water']},
+]
