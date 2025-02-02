69a39fc93e6a569ee2f9317668f46e0d01f84f55
Inge-dev <ingelewerentz@gmail.com>
2025-02-01 20:35:31 -0800

    Wrapped PokemonList in an IIFE
    Updated .forEach loop replacing pokemonList with new variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,31 @@
-let pokemonList= [
-    {name: 'Jigglypuff', height: 5, type: ['poison', 'steel']},
-    {name: 'Butterfree', height: 10, type: ['rock', 'electric']},
-    {name: 'Charmander', height: 6, type: ['ground', 'water']},
-];
-
-pokemonList.forEach(function(pokemon){
-if(pokemon.height > 6){ 
-    document.write ("<p>" +  pokemon.name + " height: " + pokemon.height + " type: " + pokemon.type +  " What a big pokemon! " + "<p>");
-}else{
-    document.write ("<p>" +  pokemon.name + " height: " + pokemon.height + " type: " + pokemon.type + "<p>");
-}
-});
-
-
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Jigglypuff', height: 5, type: ['poison', 'steel']},
+        {name: 'Butterfree', height: 10, type: ['rock', 'electric']},
+        {name: 'Charmander', height: 6, type: ['ground', 'water']},
+    ];
   
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+
+pokemonRepository.add ({ name: 'Rapidash' });
+  
+pokemonRepository.getAll().forEach(function(pokemon){ // Updated .forEach loop to access pokemonList from within function
+    if(pokemon.height > 6){ 
+        document.write ("<p>" +  pokemon.name + " height: " + pokemon.height + " type: " + pokemon.type +  " What a big pokemon!" + "<p>");
+    }else{
+        document.write ("<p>" +  pokemon.name + " height: " + pokemon.height + " type: " + pokemon.type + "<p>");
+    }
+    });
+
